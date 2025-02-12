9ca121f589023a0d624085d7fbb8be2e092cb942
Merve Ozel <leylakmerve@gmail.com>
2025-02-12 13:37:07 +0300

    Basketarea updated

--- assets/js/details.js
+++ assets/js/details.js
@@ -1,66 +1,62 @@
 let card = [];
-let totalPrice = "";
+let totalPrice = 0;
 const overlay = document.querySelector(".overlay");
 let addBtn = document.querySelector('.addBtn').addEventListener('click', function () {
   overlay.style.display = "block";
   myDialog.showModal();
   const productNames = productName.innerText
   const productPrices = productPrice.innerText
+  const priceNumber = parseFloat(productPrices.replace('$', '').replace(',', ''));
+  console.log(priceNumber);
   const productPhotos = productPhoto.src
-  card.push({ name: productNames, price: productPrices, quantity: 1, image: productPhotos })
-  totalPrice += productPrices
+  const foundedProduct = card.find(x => x.name === productNames)
+  if (foundedProduct) {
+    foundedProduct.quantity += 1
+  } else {
+    card.push({ name: productNames, price: priceNumber, quantity: 1, image: productPhotos })
+  }
   console.log(card)
+  console.log(foundedProduct)
   updateCard();
 })
 
 function updateCard() {
+
   let cardLength = card.length
-  myDialog.innerHTML=`
-          <div class="dialog-top">
-          <h3>Cart <span>(0)</span></h3>
-          <button class="remove-all" id="removeBtn">Remove All</button>
-        </div>
-        </div>
-        <div class="dialog-bottom">
-          <h6>TOTAL</h6>
-          <span id="total" > </span>
-        </div>
-    <button class="checkout-btn">CHECKOUT</button>
-  `
+  const cardLengthHtml = document.querySelector('.cardLength').innerText = `(${cardLength})`
+  const productList = document.querySelector('.dialog-product-part');
+  productList.innerHTML = ""
   card.forEach(x => {
-    myDialog.innerHTML = `
-        <div class="dialog-top">
-          <h3>Cart <span>(${cardLength})</span></h3>
-          <button class="remove-all" id="removeBtn">Remove All</button>
-        </div>
-        <div class="dialog-product-part">
-          <div class="product-left">
-            <img src=${x.image} alt="">
-          </div>
-          <div class="product-middle" >
-            <h2>${x.name}</h2>
-            <h4>${x.price}</h4>
-          </div>
-          <div class="product-right">
-            <button class="btn decrement">-</button>
-            <span class="counter">0</span>
-            <button class="btn increment">+</button>
-          </div>
-        </div>
-        <div class="dialog-bottom">
-          <h6>TOTAL</h6>
-          <span id="total"> </span>
-        </div>
+    totalPrice = (x.price * x.quantity)
+    const formattedTotalPrice = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      maximumFractionDigits: 0
+    }).format(totalPrice);
+    productList.innerHTML = `
+      <div class="product-left">
+        <img src=${x.image} alt="">
+      </div>
+      <div class="product-middle" >
+        <h2>${x.name}</h2>
+        <h4>${x.price}</h4>
+      </div>
+      <div class="product-right">
+        <button class="btn decrement">-</button>
+        <span class="counter">${x.quantity}</span>
+        <button class="btn increment">+</button>
+      </div>
+
       `
+    document.getElementById('total').innerText = `${formattedTotalPrice} `
   });
-  document.getElementById('total').innerText= `${totalPrice} `
-  removeBtn.addEventListener('click', function(){
+  removeBtn.addEventListener('click', function () {
     card = [];
-    totalPrice ="0";
+    totalPrice = "0";
     updateCard()
 
   })
- 
+
 }
 
 // close the dialog with overlay
