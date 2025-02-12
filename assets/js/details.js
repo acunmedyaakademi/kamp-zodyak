ddedaab156e5a585cc2b1465ca3757f0634138c7
Merve Ozel <leylakmerve@gmail.com>
2025-02-12 09:47:20 +0300

    dialog overlay added. Dialog close event added

--- assets/js/details.js
+++ assets/js/details.js
@@ -1,14 +1,30 @@
 let card=[];
 let totalPrice = 0;
-
+const overlay = document.querySelector(".overlay");
 let addBtn = document.querySelector('.addBtn').addEventListener('click', function(){
+  overlay.style.display = "block";
   myDialog.showModal();
   const productNames = productName.innerText
   const productPrices = productPrice.innerText
   console.log(productNames)
   card.push({name:productNames, price:productPrices})
 })
+// close the dialog with overlay
+myDialog.addEventListener("click", (event) => {
+  const dialogBounds = myDialog.getBoundingClientRect();
+  const isInsideDialog = (
+    event.clientX >= dialogBounds.left &&
+    event.clientX <= dialogBounds.right &&
+    event.clientY >= dialogBounds.top &&
+    event.clientY <= dialogBounds.bottom
+  );
 
+  if (!isInsideDialog) {
+    myDialog.close();
+    overlay.style.display = "none";
+  }
+});
+// end the close event
 
 let basketBtn = document.querySelector('.basketBtn').addEventListener('click', function(){
   myDialog.showModal();
